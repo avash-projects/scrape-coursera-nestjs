3b9491b4ae3f29ba247f9d9fca6041cc3df09bfa
avash <avash700@gmail.com>
2023-01-21 10:47:17 +0545

    update: scrape init

--- src/puppeteer/puppeteer.service.ts
+++ src/puppeteer/puppeteer.service.ts
@@ -4,13 +4,21 @@ import { stringify } from 'querystring';
 
 @Injectable()
 export class PuppeteerService {
-    ipEl: string;
-    btnEl: string;
-    constructor(){
-        this.ipEl='#rendered-content > div > header > div > div > div > div:nth-child(1) > div.rc-SearchBarContainer > div > form > div > div > div.rc-AutoComplete.horizontal-box.isLohpRebrand > div.react-autosuggest__container > input';
-        this.btnEl='#rendered-content > div > header > div > div > div > div:nth-child(1) > div.rc-SearchBarContainer > div > form > div > div > div.rc-AutoComplete.horizontal-box.isLohpRebrand > button.nostyle.search-button > div';
+    ipSel: string;
+    btnSel: string;
+    resSel: string;
+
+    constructor() {
+        this.ipSel = '#rendered-content > div > header > div > div > div > div:nth-child(1) > div.rc-SearchBarContainer > div > form > div > div > div.rc-AutoComplete.horizontal-box.isLohpRebrand > div.react-autosuggest__container > input';
+        this.btnSel = '#rendered-content > div > header > div > div > div > div:nth-child(1) > div.rc-SearchBarContainer > div > form > div > div > div.rc-AutoComplete.horizontal-box.isLohpRebrand > button.nostyle.search-button > div';
+        this.resSel = ".css-ilhc4l";
     }
-    async scrapeCoursera(category: string) {   
+
+    async scrapeCoursera(category: string) {
+        const {page,browser} = await this.searchCoursera(category);
+    }
+
+    async searchCoursera(category: string) {
         const browser = await puppeteer.launch({
             headless: false,
             // defaultViewport: false,
@@ -22,18 +30,21 @@ export class PuppeteerService {
             deviceScaleFactor: 1,
         })
         await page.goto('https://www.coursera.org/', {
-            waitUntil: 'domcontentloaded'
+            waitUntil: 'networkidle2'
         });
         // Type into search box.
-        const inputEl = await page.waitForSelector(this.ipEl);
+        const inputEl = await page.waitForSelector(this.ipSel);
         await inputEl.type(category);
-        const searchBtn = await page.waitForSelector(this.btnEl);
+        const searchBtn = await page.waitForSelector(this.btnSel);
         await searchBtn.click({
             offset: {
                 x: 5,
                 y: 5
             }
-        })
-        // await browser.close();
+        });
+        await page.waitForNavigation();
+        return { browser, page };
     }
+
+
 }
